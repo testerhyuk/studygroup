5f7043dbf7388f740efbba7a63b6a5de83850f84
Hyuk <testerhyuk214@gmail.com>
2025-06-17 14:34:47 +0900

    study db설정 및 snowflake 구현

--- backend/service/study/src/main/java/hyuk/studygroup/StudyApplication.java
+++ backend/service/study/src/main/java/hyuk/studygroup/StudyApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArticleApplication {
+public class StudyApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ArticleApplication.class, args);
+        SpringApplication.run(StudyApplication.class, args);
     }
 }
